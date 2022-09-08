e1962dc89c27b9a25d972a7d335e58bae41d2eb1
Mavinkip <mavinngetich1@gmail.com>
2022-09-08 14:05:54 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #!/bin/bash
 #include <stdio.h>
+
 /**
  *main - print the string in the put function
  *
@@ -9,6 +10,6 @@
  */
 int main (void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+           puts("\"Programming is like building a multilingual puzzle");
+           return (0);
 }
