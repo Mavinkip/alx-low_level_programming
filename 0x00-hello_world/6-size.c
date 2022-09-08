453add9dc39431b2cb5601befdbe11f57bec028c
Mavinkip <mavinngetich1@gmail.com>
2022-09-08 15:16:38 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the printf function
+ * main print string in print function
  *
- * Description: using the main function 
- * this program prints with proper grammar, but the outcome is a piece of art, 
- * Return: 0 
+ * Description: using the main function
+ * this program prints size of various types
+ * Return: 0
  */
 int main(void)
 {
