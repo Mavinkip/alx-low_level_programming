3d65661bc305b7191c16d91f5872a0d101050fe2
Mavinkip <mavinngetich1@gmail.com>
2022-09-08 15:09:42 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,6 +18,6 @@ int main(void)
 	printf("size of an int: %ld byte(s)\n", sizeof(i));
 	printf("size of a long int: %ld byte(s)\n", sizeof(ll)); 
 	printf("size of a long long int: %ld byte(s)\n", sizeof(lll));
-	printf("size of a float int: %ld byte(s)\n", sizeof(f)); 
+	printf("size of a float: %ld byte(s)\n", sizeof(f)); 
 	return (0);
 }
