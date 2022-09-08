766f6e741d232cc5f7578a74d90c70697bf09c00
Mavinkip <mavinngetich1@gmail.com>
2022-09-08 14:56:46 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#incude <stdio.h>
+
+/**
+ * main - print the string in the printf function
+ *
+ * Description: using the main function 
+ * this program prints with proper grammar, but the outcome is a piece of art, 
+ * Return: 0 
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long ll;
+	long long lll;
+	float f;
+	printf("size of char: %ld byte(s)\n", sizeof(c));
+	printf("size of int: %ld byte(s)\n", sizeof(i));
+	printf("size of long long: %ld byte(s)\n", sizeof(ll));
+	printf("size of long long int: %ld byte(s)\n", sizeof(lll));
+	printf("size of float int: %ld byte(s)\n", sizeof(f)); 
+	return (0);
+}
