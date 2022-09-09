3d7281121b3343275852e2ca70468d1e1429bc82
Mavinkip <mavinngetich1@gmail.com>
2022-09-09 07:17:56 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,42 @@
+nclude <stdlib.h>
+
+#include <time.h>
+
+/* more headers goes there */
+
+
+
+/**
+ * main - print the string in the printf function 
+ *
+ * Description: using the main function 
+ * this program prints size of "Programming is like building a multilingual puzzle
+ * Return: 0 
+ */
+int main(void)
+
+{
+
+		int n;
+
+
+
+			srand(time(0));
+
+				n = rand() - RAND_MAX / 2;
+
+				if (n % 10 > 5)
+				{
+					printf("last digit of %i is %i and is greater than 5\n", n, n % 10);
+				}
+				else if (n % 10 == 0)
+				{
+					printf("last digit of %i is %i and is 0\n", n, n % 10);
+				}
+				else
+				{
+					printf("last digit %i is %i and is less than 6 and not 0\n", n, n% 10);
+				}
+					return (0);
+
+}
