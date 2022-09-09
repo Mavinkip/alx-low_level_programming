20ece43a576ba6196a8a149e2984e7ae142feb7f
Mavinkip <mavinngetich1@gmail.com>
2022-09-09 11:45:49 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,38 +1,55 @@
 #include <stdio.h>
-#include <time.h>
+
 #include <stdio.h>
 
+#include <time.h>
+
+
+
 /**
- * main - print the string in the printf function 
  *
- * Description: using the main function 
- * this program prints size of "Programming is like building a multilingual puzzle
- * Return: 0 
- */
-int main(void)
+ * * main - Entry point
+ *
+ * * 
+ *
+ * * Return: Always 0 (success)
+ *
+ * */int main(void)
 
 {
 
-		int n;
+	  int n;
+
+	    int L;
+
+	      char str[] = "last digit of";
+
+	        
+
+	       srand(time(0));
+
+	        n = rand() - RAND_MAX / 2;
+
+		 L = n % 10;
 
 
 
-			srand(time(0));
+		   if (L > 5)
 
-				n = rand() - RAND_MAX / 2;
+			       printf("%s %d is %d and is greater than 5\n", str, n, l);
 
-				if (n % 10 > 5)
-				{
-					printf("last digit of %i is %i and is greater than 5\n", n, n % 10);
-				}
-				else if (n % 10 == 0)
-				{
-					printf("last digit of %i is %i and is 0\n", n, n % 10);
-				}
-				else
-				{
-					printf("last digit %i is %i and is less than 6 and not 0\n", n, n% 10);
-				}
-					return (0);
+		     else if (l > 5)
+
+			         printf(%s %d is %d and is 0\n", str, n, l);
+
+		       else if (l < 6)
+
+			           printf(%s %d is %d and is less than 6 and not o\n", str, n, l);
+
+
+
+		           return (0);
 
 }
+
+
