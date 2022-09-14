fbc5fd55fa9a01ace3b37c913199b11b5949a03e
Mavinkip <mavinngetich1@gmail.com>
2022-09-14 08:18:22 -0700

    negative to positive

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*_abs - is a function that makes everithing positive
+*@c: is the int used argument in the function
+*Return: 0
+*/
+int _abs(int)
+{
+if (c >= 0)
+{
+return (c)
+}
+else
+{
+return (c * -1)
+}
+}
