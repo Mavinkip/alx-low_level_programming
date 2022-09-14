ee63deeabe1759ca3e5b08a7d32be0cec1ddc206
Mavinkip <mavinngetich1@gmail.com>
2022-09-14 07:50:11 -0700

    + 0 -

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,14 +11,14 @@ if (n > 0)
 	return (1);
 _putchar('+');
 }
-else if (n == 0)
-{
-	return (0);
-_putchar('0');
-}
-else
+else if (n < 0)
 {
 	return (-1);
 _putchar('-');
+}
+else
+{
+	return (0);
+_putchar('0');
 }
 }
