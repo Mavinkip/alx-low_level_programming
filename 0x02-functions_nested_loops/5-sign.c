c638dd739f655db6f2522727c34c34d3a7c30997
Mavinkip <mavinngetich1@gmail.com>
2022-09-14 07:24:05 -0700

    + 0 -

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*print_sign - print the signs
+*@n: is the int used inthe argument of the function
+*Return: 0
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+return (1);
+_putchar(+);
+}
+else if (n == 0)
+{
+return (0);
+_putchar(0);
+}
+else
+{
+return (-1);
+_putchar(-);
+}
+}
