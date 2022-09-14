87bdc493ed2f4f911f6d07fceb1de4a925b04950
Mavinkip <mavinngetich1@gmail.com>
2022-09-14 11:03:50 -0700

    24 hr

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,15 +7,18 @@ void jack_bauer(void)
 {
 int h;
 int m;
+int hr;
+int mr;
 
 for (h=0; h <= 23; h++)
 {
-_putchar(h);
-}
-_putchar(':');
 for (m=0; m <= 59; m++)
-{
-_putchar(m);
-}
+hr = h % 10;
+mr = m % 10;
+_putchar(h / 10 '0');
+_putchar(hr + '0');
+_putchar(':');
+_putchar(m / 10 '0');
+_putchar(mr + '0');
 _putchar('\n');
 }
