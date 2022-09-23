3203c3572d34b1e2a17f3150eb237e4fa28043f3
Mavinkip <mavinngetich1@gmail.com>
2022-09-23 08:08:27 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,17 +1,14 @@
 #include "main.h"
+#include <string.h>
 /**
-*_strncat - Concatenates two strings using at
-*@dest:The string to be appended upon.*@src: The string to be appended to dest.
-*@n: The number of bytes from src to be appended to dest.
-*@src: tge apended
-*Return: A pointer to the resulting string dest.
-*/
-char *_strncat(char *dest, char *src, int n)
+ * _strncpy - this is for copy
+ * @dest: param1
+ * @src: param2
+ * @n: param3
+ * Return: a string
+ */
+char *_strncpy(char *dest, char *src, int n)
 {
-int index = 0, dest_len = 0;
-while (dest[index++])
-dest_len++;
-for (index = 0; src[index] && index < n; index++)
-dest[dest_len++] = src[index];
-return (dest);
+    strncpy(dest, src, n);
+    return (dest);
 }
