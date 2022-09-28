10f7065d8d4f290c3a1e0a5a99a2c0d2902c993b
Mavinkip <mavinngetich1@gmail.com>
2022-09-28 12:11:21 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*_puts_recursion(char *s) - prints a string
+*@s: is the string
+*Return:0
+*/
+void _puts_recursion(char *s)
+{
+_puts_recursion("print a recution string");
+return (0);
+}
